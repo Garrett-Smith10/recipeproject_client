95f862ee0369c41bdfd0f0b23bde02438339c524
Garrett Smith <GSmith6719@gmail.com>
2024-07-26 15:11:12 -0500

    fixed some logic issues

--- src/components/pages/MyRecipes.jsx
+++ src/components/pages/MyRecipes.jsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
 import { fetchRecipes } from "../../services/recipeServices.js";
+import { useNavigate } from "react-router-dom";
 
 export const MyRecipes = () => {
   const [recipes, setRecipes] = useState([]);
   const token = localStorage.getItem("auth_token");
+  const navigate = useNavigate
 
   useEffect(() => {
     fetchRecipes(setRecipes, token);
   }, [token, setRecipes]);
+
+  const handleViewRecipe = (id) => {
+    navigate(`/myrecipes/${id}`);
+  };
 
    return (
     <div>
@@ -17,6 +23,7 @@ export const MyRecipes = () => {
           <li key={recipe.id}>
             <h3>{recipe.name}</h3>
             <img src={recipe.image} alt={recipe.name} style={{ width: "200px", height: "auto" }} />
+            <button onClick={() => handleViewRecipe(recipe.id)}>View Recipe</button>
           </li>
         ))}
       </ul>
